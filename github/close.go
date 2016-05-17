817ae8c89b686c36ad8fd3fd666521f7f95e08de
fletcher.davis <fletcher.davis@jibjab.com>
2016-05-16 21:01:49 -0700

    first pass at close issue

---
+++ github/close.go
@@ -0,0 +1,45 @@
+package github
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func CloseIssue(repo, number string) error {
+	resp, err := http.NewRequest("PATCH", CloseIssuesURL+
+		"/"+repo+"/issues/"+number+"?state=closed")
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set(
+		"Accept", "application/vnd.github.v3.text-match+json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	//!-
+	// For long-term stability, instead of http.Get, use the
+	// variant below which adds an HTTP request header indicating
+	// that only version 3 of the GitHub API is acceptable.
+	//
+	//   req, err := http.NewRequest("GET", IssuesURL+"?q="+q, nil)
+	//   if err != nil {
+	//       return nil, err
+	//   }
+	//   req.Header.Set(
+	//       "Accept", "application/vnd.github.v3.text-match+json")
+	//   resp, err := http.DefaultClient.Do(req)
+	//!+
+
+	// We must close resp.Body on all execution paths.
+	// (Chapter 5 presents 'defer', which makes this simpler.)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("close issue %s failed: %s", number, resp.Status)
+	}
+
+	resp.Body.Close()
+	return &result, nil
+}
+
+//!-
